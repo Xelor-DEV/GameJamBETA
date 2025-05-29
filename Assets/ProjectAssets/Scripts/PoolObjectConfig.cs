1b700a57eb05183e3a4cf73abfcdf7013bc51018
Miguel Angel Villanueva Alejo <140673613+Xelor-DEV@users.noreply.github.com>
2025-05-29 12:34:41 -0500

    Object Pooling Test and Audio Mixer Configuration

---
+++ Assets/ProjectAssets/Scripts/PoolObjectConfig.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "PoolObjectConfig", menuName = "ScriptableObjects/ObjectPool/PoolObjectConfig")]
+public class PoolObjectConfig : ScriptableObject
+{
+    [SerializeField] private GameObject prefab;
+
+    public GameObject Prefab
+    {
+        get
+        {
+            return prefab;
+        }
+        set
+        {
+            prefab = value;
+        }
+    }
+}
